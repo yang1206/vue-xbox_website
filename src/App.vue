ea0e82447f6d808f559234845b1dbda080424718
yang1206 <yang15188280673@outlook.com>
2022-11-21 22:34:29 +0800

    feat: 🚀 meta

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+useHead({
+  title: 'Xbox Official Site: Consoles, Games, and Community | Xbox',
+  meta: [
+    {
+      name: 'description',
+      content: 'Using Vue3,Vite,UnoCSS,Vite-SSG',
+    },
+  ],
+})
+</script>
 <template>
   <div h-full overflow-y-scroll>
     <NavBar />
